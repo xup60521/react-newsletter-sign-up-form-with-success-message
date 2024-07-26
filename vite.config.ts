7242c79ec75cdc9823e135da576e06362543f3ed
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-07-26 13:56:20 +0800

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/react-newsletter-sign-up-form-with-success-message",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
